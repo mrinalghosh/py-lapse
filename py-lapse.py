6b0001b6af01f1c43f3978dbb4b394286479d328
Mrinal Ghosh <mrinal.ghosh6198@gmail.com>
2019-06-05 11:54:15 -0400

    Added gamma

--- py-lapse.py
+++ py-lapse.py
@@ -1,14 +1,20 @@
 import cv2
 import os
 from argparse import ArgumentParser
+import numpy as np
+
 
 def lapse(folder: str = None,
           width: int = None,
           height: int = None,
           fps: float = None,
-          name: str = None
+          name: str = None,
+          gamma: float = None
           ):
-    """convert numbered png files to mp4"""
+    '''convert sequential png files to mp4'''
+
+    invgamma = 1 / gamma
+    table = np.array([((i / 255.0) ** invgamma) * 255 for i in np.arange(0, 256)]).astype('uint8')
 
     fourcc = 0x7634706d
     images = [img for img in os.listdir(folder) if img.endswith('.png')]
@@ -23,7 +29,7 @@ def lapse(folder: str = None,
 
     for image in images:
         im = cv2.imread(os.path.join(folder, image))
-        res = cv2.resize(im, (width, height), interpolation=cv2.INTER_AREA)
+        res = cv2.resize(cv2.LUT(im, table), (width, height), interpolation=cv2.INTER_AREA)
         video.write(res)
 
     cv2.destroyAllWindows()
@@ -35,9 +41,10 @@ if __name__ == '__main__':
     p.add_argument('folder', type=str, help='folder containing images')
     p.add_argument('-w', '--width', type=int, help='width in pixels', default=None)
     p.add_argument('-l', '--height', type=int, help='height in pixels', default=None)
-    p.add_argument('-f', '--fps', type=float, help='frames per second', default=5)
+    p.add_argument('-f', '--fps', type=float, help='frames per second', default=20)
     p.add_argument('-n', '--name', type=str, help='output name with .mp4 ext', default=None)
+    p.add_argument('-g', '--gamma', type=float, default=1.0)
 
     P = p.parse_args()
 
-    lapse(folder=P.folder, width=P.width, height=P.height, fps=P.fps, name=P.name)
+    lapse(folder=P.folder, width=P.width, height=P.height, fps=P.fps, name=P.name, gamma=P.gamma)
